82f46e95ee603535ef259ebe233a81b6f4c3dece
cmlee <bal.dongdongdong@gmail.com>
2021-07-18 22:21:56 +0900

    add createUser resolver

--- src/user/user.mutation.ts
+++ src/user/user.mutation.ts
@@ -1,5 +1,18 @@
+import client from '../client';
+
 export default {
   Mutation: {
-    
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      const existUser = await client.user.findFirst({
+        where: {
+          OR: [{ username }, { email }],
+        },
+      });
+
+      if (existUser) throw new Error('Already exist username or email');
+    },
   },
 };
