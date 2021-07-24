950736405a7c74f07d73681f609d1838beb00542
cmlee <bal.dongdongdong@gmail.com>
2021-07-24 18:39:31 +0900

    (feature) create account, see profile

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-const client = new PrismaClient({
+const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
 });
 
-export default client;
+export default prisma;
--- src/user/user.mutation.ts
+++ src/user/user.mutation.ts
@@ -1,18 +1,29 @@
+import bcrypt from 'bcrypt';
 import client from '../client';
 
 export default {
   Mutation: {
-    createAccount: async (
-      _,
-      { firstName, lastName, username, email, password }
-    ) => {
-      const existUser = await client.user.findFirst({
+    createAccount: async (_, { firstName, lastName, username, email, password }) => {
+      const existingUser = await client.user.findFirst({
         where: {
           OR: [{ username }, { email }],
         },
       });
 
-      if (existUser) throw new Error('Already exist username or email');
+      if (existingUser) {
+        throw new Error('This username/email is already taken.');
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      return client.user.create({
+        data: {
+          email,
+          username,
+          firstName,
+          lastName,
+          password: hashedPassword,
+        },
+      });
     },
   },
 };
