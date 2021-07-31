32e2f72aa23a408d54aeb11afb0fb8baf6118879
cmlee <bal.dongdongdong@gmail.com>
2021-07-31 20:12:47 +0900

    (fix) update token contain

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ import schema from './schema';
 
 const server = new ApolloServer({
   schema,
+  context: {
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI3NjM4MTY5fQ.2InwTQe1g22H1q_LIpL1agqb61fiQViNI_WyLbl_1Jg',
+  },
 });
 
 const PORT = process.env.PORT;
--- src/user/editProfile/editProfile.resolver.ts
+++ src/user/editProfile/editProfile.resolver.ts
@@ -1,13 +1,27 @@
-import client from '../../client';
 import bcrypt from 'bcrypt';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import client from '../../client';
 import { PASSWORD_HASH_ROUND } from '../../common/constant';
 
 export default {
   Mutation: {
-    editProfile: async (_, { firstName, lastName, username, email, password }) => {
+    editProfile: async (_, { firstName, lastName, username, email, password }, context) => {
       try {
+        console.log(context);
+        const verifiedToken = jwt.verify(
+          context.token,
+          process.env.TOKEN_SECRET_KEY || ''
+        ) as JwtPayload;
+
+        if (!verifiedToken.id) {
+          return {
+            ok: false,
+            error: 'Could not update profile.',
+          };
+        }
+
         await client.user.update({
-          where: { id: 1 },
+          where: { id: verifiedToken.id },
           data: {
             firstName,
             lastName,
